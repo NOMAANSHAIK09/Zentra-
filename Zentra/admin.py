94b9ed1ee821ebed602632e495fe38a7e498c729
nomaan shaik <nomaanshaiknomaanshaik@gmail.com>
2026-08-19 22:35:56 +0530

    LOgin and Signup is done

--- Zentra/admin.py
+++ Zentra/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from Zentra.models import userinfo
 
 # Register your models here.
+
+admin.site.register(userinfo)
--- Zentra/views.py
+++ Zentra/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from Zentra.models import userinfo
 
 # Create your views here.
 
@@ -7,9 +9,30 @@ def index(request):
     return render(request, 'index.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = userinfo.objects.get(email=email, password=password)
+            # User found, redirect to home page
+            return redirect('home')
+        except userinfo.DoesNotExist:
+            # User not found, display an error message
+            error_message = "Invalid email or password"
+            return render(request, 'login.html', {'error_message': error_message})
     return render(request, 'login.html')
 
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        users=userinfo(name=name, email=email, password=password)
+        users.save()
+        
+        return redirect('home')  
+    
     return render(request, 'signup.html')
 
 def home(request):
